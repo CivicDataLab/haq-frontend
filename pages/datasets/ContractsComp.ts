4c1b7c299a3b62deb06ab9c65175e5852ac9a889
aashimgarg <18bcs001@smvdu.ac.in>
2022-02-02 12:18:47 +0530

    grid layout for the data contracts

---
+++ pages/datasets/ContractsComp.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+const CotractsComp = styled.div`
+   margin-top :1rem;
+   display: grid;
+   grid-template-columns: repeat(4, 1fr);
+   grid-template-rows: 1fr;
+   grid-column-gap: 20px;
+
+  .contractsComp__filter {
+    grid-area: 1 / 1 / 2 / 2;
+  }
+
+  .contractsColumn {
+    grid-area: 1 / 2 / 2 / 5;
+  } 
+
+  .contractsComp__sortRow {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: 1fr;
+    grid-column-gap: 10px;
+    grid-row-gap: 1px;
+    margin-top : 1rem;
+  }
+    
+    .contractsComp__total { 
+        grid-area: 1 / 1 / 2 / 5;
+        position:relative;
+        top:20%; 
+    }
+    
+    .contractsComp__dropdown { 
+        grid-area: 1 / 5 / 2 / 6; 
+    } 
+ `
+ ;
+
+export default CotractsComp;
