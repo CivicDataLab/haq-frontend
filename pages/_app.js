90dd2e1b2412ca159cf8f071512970a942212139
PixeledCode <ashoaib540918@gmail.com>
2022-02-18 13:39:59 +0530

    page loading progress bar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
+import NextNprogress from 'nextjs-progressbar';
 import { GlobalStyle } from 'styles/Global';
-import Layout from "config/layout";
+import { DEFAULT_THEME } from 'config/theme';
+import Layout from 'config/layout';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <NextNprogress
+        color={DEFAULT_THEME.tertiary}
+        startPosition={0.3}
+        stopDelayMs={100}
+        height={3}
+        options={{ easing: 'ease', speed: 300, showSpinner: false }}
+      />
       <GlobalStyle />
       <Component {...pageProps} />
     </Layout>
