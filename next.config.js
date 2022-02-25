2aa4c2a766c1bacd9c938ee5dbfe7417342de297
PixeledCode <ashoaib540918@gmail.com>
2022-02-25 17:33:35 +0530

    styled components config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withTM = require('next-transpile-modules')(['echarts', 'zrender']);
 
-const config = {
-  styledComponents: true,
-};
-
-module.exports = withTM({ config });
+module.exports = withTM({
+  compiler: {
+    styledComponents: true,
+  },
+});
