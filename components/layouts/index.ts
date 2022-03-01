5bd54ff31698975dc442e271daaf187685ffa874
Shoaib Ahmed <ashoaib540918@gmail.com>
2022-03-01 19:53:35 +0530

    Update index.ts

--- components/layouts/index.ts
+++ components/layouts/index.ts
@@ -1,6 +1,5 @@
 export { default as Banner } from './Banner';
 export { default as Carousel } from './Carousel';
-export { default as FlexLayout } from './FlexLayout';
 export { default as Header } from './Header';
 export { default as Tabbed } from './Tabbed';
 export { default as Toggletip } from './Toggletip';
