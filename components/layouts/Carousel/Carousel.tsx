bafb6439042b5db7d0ba491acf68f916aa6c0727
PixeledCode <ashoaib540918@gmail.com>
2022-03-04 12:40:17 +0530

    carousel adding

--- components/layouts/Carousel/Carousel.tsx
+++ components/layouts/Carousel/Carousel.tsx
@@ -1,47 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import CarouselComp from './CarouselComp';
+// import CarouselComp from './CarouselComp';
+import useEmblaCarousel from 'embla-carousel-react';
+import styled from 'styled-components';
 
 const Carousel = ({ data }) => {
   const [position, setPosition] = useState(0);
+  const [emblaRef] = useEmblaCarousel();
 
-  useEffect(() => {
-    if (!document.querySelector('.carousel__item--current'))
-      document
-        .querySelector('#carousel-0')
-        .classList.add('carousel__item--current');
+  // useEffect(() => {
+  //   if (!document.querySelector('.carousel__item--current'))
+  //     document
+  //       .querySelector('#carousel-0')
+  //       .classList.add('carousel__item--current');
 
-    if (!document.querySelector('.carousel__nav [aria-pressed="true"]'))
-      document
-        .querySelector('.carousel__nav button')
-        .setAttribute('aria-pressed', 'true');
-  }, []);
+  //   if (!document.querySelector('.carousel__nav [aria-pressed="true"]'))
+  //     document
+  //       .querySelector('.carousel__nav button')
+  //       .setAttribute('aria-pressed', 'true');
+  // }, []);
 
-  useEffect(() => {
-    // changing position of carousel nav
-    const navButton = document.querySelector(`[data-number="${position}"]`);
-    if (navButton.getAttribute('aria-pressed') == 'false') {
-      document
-        .querySelector('.carousel__nav [aria-pressed="true"]')
-        .setAttribute('aria-pressed', 'false');
-      navButton.setAttribute('aria-pressed', 'true');
+  // useEffect(() => {
+  //   // changing position of carousel nav
+  //   const navButton = document.querySelector(`[data-number="${position}"]`);
+  //   if (navButton.getAttribute('aria-pressed') == 'false') {
+  //     document
+  //       .querySelector('.carousel__nav [aria-pressed="true"]')
+  //       .setAttribute('aria-pressed', 'false');
+  //     navButton.setAttribute('aria-pressed', 'true');
 
-      document
-        .querySelector(`.carousel__item--current`)
-        .classList.remove('carousel__item--current');
-      document
-        .querySelector(`#carousel-${position}`)
-        .classList.add('carousel__item--current');
-    }
+  //     document
+  //       .querySelector(`.carousel__item--current`)
+  //       .classList.remove('carousel__item--current');
+  //     document
+  //       .querySelector(`#carousel-${position}`)
+  //       .classList.add('carousel__item--current');
+  //   }
 
-    // changing the slide
-    document
-      .querySelector(`.carousel__item--current`)
-      .classList.remove('carousel__item--current');
-    document
-      .querySelector(`#carousel-${position}`)
-      .classList.add('carousel__item--current');
-  }, [position]);
+  //   // changing the slide
+  //   document
+  //     .querySelector(`.carousel__item--current`)
+  //     .classList.remove('carousel__item--current');
+  //   document
+  //     .querySelector(`#carousel-${position}`)
+  //     .classList.add('carousel__item--current');
+  // }, [position]);
 
   function updateCarousel(n: number) {
     if (n == -1 && position == 0) setPosition(data.length - 1);
@@ -56,9 +59,8 @@ const Carousel = ({ data }) => {
   }
 
   return (
-    <CarouselComp className="carousel">
-      <div className="container">
-        <button
+    <Wrapper className="embla" ref={emblaRef}>
+      {/* <button
           className="carousel__prev"
           onClick={() => updateCarousel(-1)}
           type="button"
@@ -81,29 +83,26 @@ const Carousel = ({ data }) => {
               d="M21 29h11.17l-4.88 4.88c-.39.39-.39 1.03 0 1.42.39.39 1.02.39 1.41 0l6.59-6.59c.39-.39.39-1.02 0-1.41l-6.58-6.6c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L32.17 27H21c-.55 0-1 .45-1 1s.45 1 1 1Z"
             />
           </svg>
-        </button>
-        <ul className="carousel__content" aria-live="polite">
-          {data.map((item, index) => {
-            return (
-              <li
-                key={`carousel-${index}`}
-                id={`carousel-${index}`}
-                className="carousel__item"
-              >
-                {/* <figure>
-                  <Image src={item.image} alt="" width={176} height={176} />
-                </figure> */}
-                <div>
-                  <p>{item.text}</p>
-                  <a className="btn-primary" href={item.link}>
-                    Read More
-                  </a>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-        <button
+        </button> */}
+      <ul className="embla__container carousel__content" aria-live="polite">
+        {data.map((item, index) => {
+          return (
+            <li
+              key={`carousel-${index}`}
+              id={`carousel-${index}`}
+              className="embla__slide carousel__item"
+            >
+              <div>
+                <p>{item.text}</p>
+                <a className="btn-primary" href={item.link}>
+                  Read More
+                </a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      {/* <button
           className="carousel__next"
           onClick={() => updateCarousel(1)}
           type="button"
@@ -126,25 +125,21 @@ const Carousel = ({ data }) => {
               d="M21 29h11.17l-4.88 4.88c-.39.39-.39 1.03 0 1.42.39.39 1.02.39 1.41 0l6.59-6.59c.39-.39.39-1.02 0-1.41l-6.58-6.6c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L32.17 27H21c-.55 0-1 .45-1 1s.45 1 1 1Z"
             />
           </svg>
-        </button>
-        <ul className="carousel__nav">
-          {data.map((item, index) => {
-            return (
-              <li key={`carouselNav-${index}`}>
-                <button
-                  aria-pressed="false"
-                  onClick={handleCarouselNav}
-                  data-number={index}
-                >
-                  <span className="sr-only">Slide:</span> {index + 1}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </CarouselComp>
+        </button> */}
+    </Wrapper>
   );
 };
 
 export default Carousel;
+
+const Wrapper = styled.div`
+  overflow: hidden;
+
+  .embla__container {
+    display: flex;
+  }
+  .embla__slide {
+    position: relative;
+    flex: 0 0 100%;
+  }
+`;
