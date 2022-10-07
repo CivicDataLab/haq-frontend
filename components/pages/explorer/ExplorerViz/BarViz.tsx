76e331c915f0a5f0b352ec9f7fac4dabe134f9a9
aashimgarg <18bcs001@smvdu.ac.in>
2022-10-07 12:03:47 +0530

    merge two district for same fiscal year in a bar

--- components/pages/explorer/ExplorerViz/BarViz.tsx
+++ components/pages/explorer/ExplorerViz/BarViz.tsx
@@ -52,19 +52,25 @@ const BarViz = ({ meta, data, consList }) => {
     if (state.value.length > 0 && Object.keys(data).length) {
       // for compare section
       if (state.value.length > 1) {
-        const barValues1 = [items[0].title];
-        const barValues2 = [items[1].title];
-
-        const headerArr = ['Constituency'];
-        Object.keys(data).map((year) => {
-          headerArr.push(year);
-          barValues1.push(data[year][items[0].consCode]);
-          barValues2.push(data[year][items[1].consCode]);
+        
+        const headerArr = ['Fiscal Years']
+        items.forEach((item) => {
+            headerArr.push(item.title)
         });
 
-        const barValues = [headerArr, barValues1, barValues2];
-
-        setBarData(barValues);
+        const barValuesArr = [];
+        
+        Object.keys(data).forEach((year) => {
+            const barValues = [year];
+            items.forEach((item) => {
+                barValues.push(data[year][item.consCode])
+                barValuesArr.push(barValues)
+            });
+        })
+        
+        const barArray = [headerArr, ...barValuesArr]
+        
+        setBarData(barArray);
       } else {
         const barValues1 = [items[0].title];
         const headerArr = ['Constituency'];
@@ -94,7 +100,7 @@ const BarViz = ({ meta, data, consList }) => {
       {items.length > 0 && barData.length > 0 && (
         <GroupBarChart
           yAxisLabel={`Value (in ${meta.unit})`}
-          xAxisLabel="Constituency"
+          xAxisLabel={items.length == 1 ? "Constituency" : "Fiscal Years"}
           theme={['#4965B2', '#ED8686', '#69BC99']}
           dataset={barData}
           stack={false}
