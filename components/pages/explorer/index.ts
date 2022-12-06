36c1dfe5e161f4c30ab03de634ff4e884b6a244e
aashimgarg <18bcs001@smvdu.ac.in>
2022-12-07 02:32:21 +0530

    summary explorer viz for summary data

--- components/pages/explorer/index.ts
+++ components/pages/explorer/index.ts
@@ -1,3 +1,4 @@
 export { default as ExplorerHeader } from './ExplorerHeader';
 export { default as ExplorerRelated } from './ExplorerRelated';
 export { default as ExplorerViz } from './ExplorerViz';
+export { default as SummaryExplorerViz } from './ExplorerViz/SummaryExplorerViz';
