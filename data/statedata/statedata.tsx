face78806635119268c243928259c28736e8e6de
aashimgarg <aashim1garg@gmail.com>
2023-07-28 18:20:31 +0530

    dummy data

---
+++ data/statedata/statedata.tsx
@@ -0,0 +1,145 @@
+
+export const header = {
+    main: 'Uncover money allocation and expenses for girl education in Uttar Pradesh',
+    sub: 'Explore Uttar Pradesh’s school education landscape through state budget data and district-wise treasuries data.',
+    state: 'bihar',
+  };
+
+  export const listCard = [
+    {
+      question:
+        'How efficiently was budget money allocated and utilised for education in Uttar Pradesh?',
+      main: 'Explore the state budget data to understand scheme-wise allocation, expenditure and utilisation of money',
+      sub: 'Some Trends in Education Expenditure in Telangana: The budget estimates (BE) for expenditure on Education, Sports, Culture and Arts was Rs 13,49,827.30 lakhs — which was 5.9% of the total budgetary expenditure by the Telangana Government (Fig 1). However, as can be seen from Fig. 1, the share of expenditure on education is consistently lower than the national average. Furthermore, the gap between the respective shares only diverged from 2014–15 to 2021–22. Figure 1 Source: Various rounds of State Finances.',
+      tag: 'Treasury',
+      imgSrc: '/assets/budgetimage.png',
+      imgAltText: 'budget',
+      buttonText: 'Explore 30+ schemes',
+    },
+    {
+      question:
+        'How efficiently was budget money allocated and utilised for education in Uttar Pradesh?',
+      main: 'Explore the state budget data to understand scheme-wise allocation, expenditure and utilisation of money',
+      sub: 'Some Trends in Education Expenditure in Telangana: The budget estimates (BE) for expenditure on Education, Sports, Culture and Arts was Rs 13,49,827.30 lakhs — which was 5.9% of the total budgetary expenditure by the Telangana Government (Fig 1). However, as can be seen from Fig. 1, the share of expenditure on education is consistently lower than the national average. Furthermore, the gap between the respective shares only diverged from 2014–15 to 2021–22. Figure 1 Source: Various rounds of State Finances.',
+      tag: 'Treasury',
+      imgSrc: '/assets/budgetimage.png',
+      imgAltText: 'budget',
+      buttonText: 'Explore 30+ schemes',
+    },
+  ];
+
+  export const obj = {
+    bihar: {
+      'all datasets': [
+        {
+          scheme: 'pmuy',
+          scheme_code: 1,
+          tag: 'budget',
+        },
+        {
+          scheme: 'pmuy-a',
+          scheme_code: 2,
+          tag: 'budget',
+        },
+        {
+          scheme: 'pmuy-b',
+          scheme_code: 3,
+          tag: 'budget',
+        },
+        {
+          scheme: 'pmuy-c',
+          scheme_code: 4,
+          tag: 'treasury',
+        },
+        {
+          scheme: 'pmuy-d',
+          scheme_code: 5,
+          tag: 'treasury',
+        },
+        {
+          scheme: 'pmuy-e',
+          scheme_code: 6,
+          tag: 'treasury',
+        },
+      ],
+      'spending data': [
+        {
+          scheme: 'pmuy',
+          scheme_code: 1,
+          tag: 'budget',
+        },
+        {
+          scheme: 'pmuy-a',
+          scheme_code: 2,
+          tag: 'budget',
+        },
+        {
+          scheme: 'pmuy-b',
+          scheme_code: 3,
+          tag: 'budget',
+        },
+      ],
+      'budget data': [
+        {
+          scheme: 'pmuy-c',
+          scheme_code: 4,
+          tag: 'budget',
+        },
+        {
+          scheme: 'pmuy-d',
+          scheme_code: 5,
+          tag: 'budget',
+        },
+        {
+          scheme: 'pmuy-e',
+          scheme_code: 6,
+          tag: 'budget',
+        },
+      ],
+    },
+    assam: {
+      'spending data': [
+        {
+          scheme: 'pmuy',
+          scheme_code: 1,
+        },
+        {
+          scheme: 'pmuy-a',
+          scheme_code: 2,
+        },
+        {
+          scheme: 'pmuy-b',
+          scheme_code: 3,
+        },
+      ],
+      'budget data': [
+        {
+          scheme: 'pmuy',
+          scheme_code: 1,
+        },
+        {
+          scheme: 'pmuy-a',
+          scheme_code: 2,
+        },
+        {
+          scheme: 'pmuy-b',
+          scheme_code: 3,
+        },
+      ],
+    },
+  };
+
+ export const stateArray = [
+    {
+      State: 'Bihar',
+      Description: 'Bihar is located in',
+    },
+    {
+      State: 'Assam',
+      Description: 'Assam is located in',
+    },
+    {
+      State: 'Uttar Pradesh',
+      Description: 'Uttar Pradesh is located in',
+    },
+  ];
