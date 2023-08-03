1298afbdfc6e5d45dcf045c60a32c40a6a8e4226
aashimgarg <aashim1garg@gmail.com>
2023-08-03 21:03:54 +0530

    dummy data for scheme-filters

---
+++ data/searchfilter/searchfilter.ts
@@ -0,0 +1,62 @@
+export const facets = {
+    scheme_mode: {
+      title: 'scheme_mode',
+      items: [
+        {
+          name: 'Payments & Awards',
+          display_name: 'Payments & Awards',
+          count: 16,
+        },
+        {
+          name: 'Others',
+          display_name: 'Others',
+          count: 2,
+        },
+        {
+          name: 'Institutional Grants',
+          display_name: 'Institutional Grants',
+          count: 76,
+        },
+        {
+          name: 'Infrastructure',
+          display_name: 'Infrastructure',
+          count: 118,
+        },
+        {
+          name: 'In-kind service delivery',
+          display_name: 'In-kind service delivery',
+          count: 44,
+        },
+        {
+          name: 'Direct Cash Transfer to students',
+          display_name: 'Direct Cash Transfer to students',
+          count: 15,
+        },
+      ],
+    },
+    scheme_type: {
+      title: 'scheme_type',
+      items: [
+        {
+          name: 'Benefits girl students exclusively',
+          display_name: 'Benefits girl students exclusively',
+          count: 6,
+        },
+        {
+          name: 'Benefits boy students exclusively',
+          display_name: 'Benefits boy students exclusively',
+          count: 8,
+        },
+        {
+          name: 'Benefits both boy and girl students indirectly',
+          display_name: 'Benefits both boy and girl students indirectly',
+          count: 150,
+        },
+        {
+          name: 'Benefits both boy and girl students directly',
+          display_name: 'Benefits both boy and girl students directly',
+          count: 107,
+        },
+      ],
+    },
+  };
