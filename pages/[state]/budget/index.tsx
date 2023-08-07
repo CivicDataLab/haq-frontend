537abfa9814745fcf43aa77abe59247640354792
aashimgarg <aashim1garg@gmail.com>
2023-08-07 17:06:10 +0530

    add header component

--- pages/[state]/budget/index.tsx
+++ pages/[state]/budget/index.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Breadcrumb } from 'components/actions';
+import { Header } from 'components/pages/budget';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import { capitalizeWords } from 'utils/data';
@@ -10,6 +11,13 @@ type Props = {
 
 const Budget: React.FC<Props> = ({ state }) => {
   const breadcrumbArray = ['Home', capitalizeWords(state), 'Budget Data'];
+
+  const header = {
+    main: ` ${capitalizeWords(state)} State Budget Data`,
+    sub: 'Welcome to our comprehensive girl education platform! We understand that you may be interested in exploring specific information related to girl education in Uttar Pradesh, Assam, or Bihar. To cater to your needs, ',
+    state: 'bihar',
+  };
+
   return (
     <>
       <Head>
@@ -18,6 +26,7 @@ const Budget: React.FC<Props> = ({ state }) => {
       <Wrapper>
         <main className="container">
           <Breadcrumb crumbs={breadcrumbArray} />
+          <Header header={header} />
         </main>
       </Wrapper>
     </>
