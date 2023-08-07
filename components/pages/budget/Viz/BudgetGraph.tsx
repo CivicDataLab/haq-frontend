0312489358813bfc4e18893fb2666945c97b0bbc
aashimgarg <aashim1garg@gmail.com>
2023-08-07 17:47:16 +0530

    "feat(graph): Implement BudgetGraph with BarChart visualization"

--- components/pages/budget/Viz/BudgetGraph.tsx
+++ components/pages/budget/Viz/BudgetGraph.tsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { GroupBarChart } from 'components/viz';
 
-const BudgetGraph = () => {
-  return <Wrapper>Bar Graph</Wrapper>;
+const BudgetGraph = ({ data, scheme_code }) => {
+  
+  const [bardata, setBarData] = useState([]);
+
+  useEffect(() => {
+  if (scheme_code) {
+    const indicatorNames = ['indicator_01', 'indicator_02', 'indicator_03'];
+    const headerArr = ['Fiscal Years', 'Budget Estimates', 'Actual Estimates', 'New Estimates'];
+    const barValuesArr = [];
+
+    const fiscalYears = Object.keys(data[indicatorNames[0]]);
+
+    fiscalYears.forEach((year) => {
+      const barValues = [year];
+      indicatorNames.forEach((indicatorName) => {
+        barValues.push(data[indicatorName][year]);
+      });
+      barValuesArr.push(barValues);
+    });
+
+    const barArray = [headerArr, ...barValuesArr];
+    setBarData(barArray);
+  }
+}, [scheme_code]);
+ 
+  return (
+    <Wrapper>
+      {bardata.length > 0 && (
+        <section className="barViz">
+          <GroupBarChart
+            yAxisLabel={`Value in lakhs `}
+            xAxisLabel="Financial Years"
+            theme={['#4965B2', '#ED8686', '#69BC99']}
+            dataset={bardata}
+            stack={false}
+            Title=""
+            subTitle=""
+            left="70vw"
+            type="bar"
+            smooth={true}
+          />
+        </section>
+      )}
+    </Wrapper>
+  );
 };
 
 export default BudgetGraph;
 
-export const Wrapper = styled.section``;
+export const Wrapper = styled.section`
+  .styles {
+    input:focus-visible {
+      outline: 0px !important;
+    }
+  }
+`;
