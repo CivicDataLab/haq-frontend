3d0735279dfe17d0fb12795af0d955d45c6c86da
aashimgarg <aashim1garg@gmail.com>
2023-08-21 14:09:42 +0530

    fix the layout for homepage

--- data/home.ts
+++ data/home.ts
@@ -6,3 +6,26 @@ export const states = [
       tags: ['Budget Data', 'Treasury Data']
     },
   ];
+
+export const summaryCards = [
+  {
+    text: 'Total Education Budget',
+    value: '₹ 103761.63 Cr.',
+  },
+  {
+    text: 'of State Budget',
+    value: '05.56%',
+  },
+  {
+    text: 'of Education Budget',
+    value: '20.63%',
+  },
+  {
+    text: 'of State Budget',
+    value: '05.56%',
+  },
+  {
+    text: 'of Education Budget',
+    value: '20.63%',
+  },
+];
